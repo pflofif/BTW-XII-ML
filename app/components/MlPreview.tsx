622916c1e1e09604b97d2cd58cbd2175a04b212f
Bohdan Vivchar <bohdan@office.cquelle.de>
2023-07-08 20:21:57 +0300

    refactor code, extract MlPreview to component

---
+++ app/components/MlPreview.tsx
@@ -0,0 +1,23 @@
+interface MlPreviewProps {
+    onClickEvent: () => void;
+    title: string
+    subtitle: string
+}
+
+const MlPreview = ({ onClickEvent, title, subtitle }: MlPreviewProps) => {
+    return (
+        <a
+            onClick={onClickEvent}
+            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+            rel="noopener noreferrer">
+            <h2 className={`mb-3 text-2xl font-semibold`}>
+                {title}{' '}
+                <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                    -&gt;
+                </span>
+            </h2>
+            <p className={`m-0 text-sm opacity-70`}> {subtitle} </p>
+        </a>
+    )
+}
+export default MlPreview;
--- app/page.tsx
+++ app/page.tsx
@@ -5,69 +5,37 @@ import DesignRespML from './DesignRespML/page';
 import ItRespML from './ItRespML/page';
 import LogisticRespML from './LogisticRespML/page';
 import QA from './components/QA';
+import MlPreview from './components/MlPreview';
 
 type ActiveMlState = 'design' | 'it' | 'logist' | 'none'
 
 export default function Home() {
   const [state, setState] = useState<ActiveMlState>('none');
+
   return (
     <main className="flex flex-col items-center lg:p-24 p-8 ">
-      <h1 className='text-2xl'>
-        Привіт Привіт, тут зібрані мотиваційні листи Боді Вівчара на проєкт BTW XII!</h1>
+      <h1 className='text-2xl'>Привіт Привіт, тут зібрані мотиваційні листи Боді Вівчара на проєкт BTW XII!</h1>
       <h2 className='text-l'>(Для початку оберіть мотивашку, яку хочите прочитати)</h2>
 
       <div className="lg:mt-20 mt-5 grid text-center lg:mb-0 lg:grid-cols-3 lg:text-left">
-        <a
-          onClick={() => { setState('design') }}
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Мотивашка на ДИЗАЙНЕРАА{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 text-sm opacity-70`}>
-            Гарний дизайн ти вже бачиш, але почитати мотивацію тоже круто!!!
-          </p>
-        </a>
+        <MlPreview
+          onClickEvent={() => { setState('design') }}
+          title='Мотивашка на ДИЗАЙНЕРАА'
+          subtitle='Гарний дизайн ти вже бачиш, але почитати мотивацію тоже круто!!!' />
 
-        <a
-          onClick={() => { setState('logist') }}
-          // href="/LogisticRespML"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Мотивашка на ЛОГІСТААА{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 text-sm opacity-70`}>
-            Розказую про жосткі схеми як зафармити на бус!!!
-          </p>
-        </a>
+        <MlPreview
+          onClickEvent={() => setState('logist')}
+          title='Мотивашка на ЛОГІІІСТААА'
+          subtitle='Розказую про жосткі схеми як зафармити на бус!!!' />
 
-        <a
-          onClick={() => { setState('it') }}
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Мотивашка на ІТ ІТ ІТ{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0  text-sm opacity-70`}>
-            Сайтик написав то і гарного мотивуючого тексту треба!!!
-          </p>
-        </a>
+        <MlPreview
+          onClickEvent={() => { setState('it') }}
+          title='Мотивашка на ІТ ІТ ІТ'
+          subtitle='Сайтик написав то і гарного мотивуючого тексту треба!!!' />
       </div>
-      <div className={`${state !== 'none' ? "rounded-lg border border-gray-700 lg:px-16 p-8" : ""}`}
-      >
+
+      <div
+        className={`${state !== 'none' ? "rounded-lg border border-gray-700 lg:px-16 p-8" : ""}`} >
         {state === 'design' ? (
           <DesignRespML />
         ) : state === 'it' ? (
@@ -76,10 +44,12 @@ export default function Home() {
           <LogisticRespML />
         ) : null}
       </div>
-      <div className='lg:pt-16 pt-8'>
+
+      <div className='lg:pt-16 pt-8'  >
         <h1 className="bg:text-2xl sm:text-xl font-black text-center">Секція питаняяяяяя - відповідьььь(на питання треба нажимати!)</h1>
         <QA />
       </div>
+
       <div className='grid lg:grid-cols-3'>
         <BTWLogo className="dark:invert" />
         <BTWLogo />

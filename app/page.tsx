de00570f19c725428a588677bf8490f6ed6c5761
Bohdan Vivchar <bohdan@office.cquelle.de>
2023-07-08 16:11:52 +0300

    add responsibiblity to mobile devices

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,11 @@ import BTWLogo from './components/BTWlogo'
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center p-24 ">
-      <h1>
+    <main className="flex flex-col items-center lg:p-24 p-8 ">
+      <h1 className='text-2xl'>
         Привіт Привіт, тут зібрані мотиваційні листи Боді Вівчара на проєкт BTW XII!
       </h1>
-      <div className="mt-20 mb-32 grid text-center lg:mb-0 lg:grid-cols-3 lg:text-left">
+      <div className="lg:mt-20 mt-5 grid text-center lg:mb-0 lg:grid-cols-3 lg:text-left">
         <a
           href="/DesignRespML"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
@@ -30,7 +30,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Мотивашка на ЛОГІІСТА{' '}
+            Мотивашка на ЛОГІСТААА{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
@@ -57,7 +57,7 @@ export default function Home() {
         </a>
 
       </div>
-      <div className='mt-35 grid lg:grid-cols-3'>
+      <div className='grid lg:grid-cols-3'>
         <BTWLogo className="dark:invert" />
         <BTWLogo />
         <BTWLogo className="dark:invert" />

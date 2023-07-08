1a70320338092ca923092e67e060f85fcda9f265
Bohdan Vivchar <bohdan@office.cquelle.de>
2023-07-08 16:02:26 +0300

    finish QA component for all MLs

--- app/components/ResponseML.tsx
+++ app/components/ResponseML.tsx
@@ -1,9 +1,35 @@
+import BTWLogo from "./BTWlogo";
+import QA from "./QA";
+
 interface RespProps {
-    text: string
+    headerText: string
+    mainText: string;
 }
-const ResponseML = ({ text }: RespProps) => {
+const ResponseML = ({ headerText, mainText }: RespProps) => {
     return (
-        <h2>{text}</h2>
+        <div>
+            <h1 className="mt-1 text-lg font-semibold sm:text-slate-900 md:text-2xl ">{headerText}</h1>
+            {/* <div className="grid grid-cols-2 grid-rows-1 gap-5">
+                <p>{mainText}</p>
+
+                <div className="grid grid-flow-col grid-cols-3 ">
+
+                    <div className="transform scale-80 -rotate-45 translate-x-40 translate-y-0">
+                        <img src="bill.jfif" alt="" loading="lazy" />
+                    </div>
+                    <div className="transform scale-40 translate-y-20 -rotate-45">
+                        <img src="musk.jfif" alt="" loading="lazy" />
+                    </div>
+                    <div className="transform translate-y-20 -rotate-45 -translate-x-20">
+                        <img src="tsuker.jpeg" alt="" loading="lazy" />
+                    </div>
+
+                </div>
+
+            </div> */}
+            <h1 className="bg:text-2xl sm:text-xl font-black text-center">Секція питаняяяяяя - відповідьььь</h1>
+            <QA />
+        </div>
     )
 }
 export default ResponseML;
